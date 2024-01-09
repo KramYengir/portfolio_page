6e04e56901dfdd002be11190bd667af394db15b2
KramYengir <kramyengir@gmail.com>
2024-01-09 17:52:56 +0000

    services basic layout complete

--- src/components/Services/services.jsx
+++ src/components/Services/services.jsx
@@ -1,7 +1,89 @@
+import { FaCheckCircle } from "react-icons/fa";
 import "./services.css";
 
 const Services = () => {
-  return <section id="services">services</section>;
+  return (
+    <section id="services">
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+      <div className="container services__container">
+        {/* ------ DESIGN ----- */}
+        <article className="service">
+          <div className="service__head">
+            <h3>Design</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <FaCheckCircle className={"service__icon"} />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <FaCheckCircle className={"service__icon"} />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <FaCheckCircle className={"service__icon"} />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <FaCheckCircle className={"service__icon"} />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+          </ul>
+        </article>
+
+        {/* ------ WEB DEV ----- */}
+        <article className="service">
+          <div className="service__head">
+            <h3>Web Development</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <FaCheckCircle className={"service__icon"} />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <FaCheckCircle className={"service__icon"} />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <FaCheckCircle className={"service__icon"} />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <FaCheckCircle className={"service__icon"} />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+          </ul>
+        </article>
+
+        {/* ------ ANALYSIS ----- */}
+        <article className="service">
+          <div className="service__head">
+            <h3>Analysis</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <FaCheckCircle className={"service__icon"} />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <FaCheckCircle className={"service__icon"} />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <FaCheckCircle className={"service__icon"} />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+            <li>
+              <FaCheckCircle className={"service__icon"} />
+              <p>Lorem ipsum dolor sit amet.</p>
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Services;
