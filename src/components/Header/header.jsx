24b3e5df9dae478254b3a4b59d44d86cbbea06bc
KramYengir <kramyengir@gmail.com>
2024-01-09 20:48:23 +0000

    tweaked header layout and began portfolio

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -2,7 +2,7 @@ import { FaArrowDown } from "react-icons/fa";
 import CTA from "./CTA";
 import HeaderSocials from "./headerSocials";
 import "./header.css";
-import myImg from "../../assets/me.jpg";
+import myImg from "../../assets/me2.jpg";
 
 const Header = () => {
   return (
@@ -16,7 +16,7 @@ const Header = () => {
         <div className="me">
           <img src={myImg} alt="Photo of Creator" />
         </div>
-        <a href="#contact" className="scroll__down">
+        <a href="#about" className="scroll__down">
           <FaArrowDown />
         </a>
       </div>
--- src/components/Portfolio/portfolio.jsx
+++ src/components/Portfolio/portfolio.jsx
@@ -1,7 +1,57 @@
 import "./portfolio.css";
+import IMG1 from "../../assets/me.jpg";
 
 const Portfolio = () => {
-  return <section id="portfolio">Porfolio</section>;
+  return (
+    <section id="portfolio">
+      <h5>Some of my Work</h5>
+      <h2>Portfolio</h2>
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__image">
+            <img src={IMG1} alt="image of project" />
+          </div>
+          <h3>Porfolio Item</h3>
+          <div className="portfolio__cta">
+            <a href="" target="blank" className="btn">
+              Github
+            </a>
+            <a href="" target="blank" className="btn btn-primary">
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__image">
+            <img src={IMG1} alt="image of project" />
+          </div>
+          <h3>Porfolio Item</h3>
+          <div className="portfolio__cta">
+            <a href="" target="blank" className="btn">
+              Github
+            </a>
+            <a href="" target="blank" className="btn btn-primary">
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__image">
+            <img src={IMG1} alt="image of project" />
+          </div>
+          <h3>Porfolio Item</h3>
+          <div className="portfolio__cta">
+            <a href="" target="blank" className="btn">
+              Github
+            </a>
+            <a href="" target="blank" className="btn btn-primary">
+              Live Demo
+            </a>
+          </div>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
