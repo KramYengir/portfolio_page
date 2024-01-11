9ea6d47c4d694b7da7cc259c59adf2fe83d198c7
KramYengir <kramyengir@gmail.com>
2024-01-11 19:26:53 +0000

    navbar now refelcts active area

--- src/components/Nav/nav.jsx
+++ src/components/Nav/nav.jsx
@@ -2,10 +2,44 @@ import "./nav.css";
 import { IoMdHome } from "react-icons/io";
 import { FaInfoCircle, FaListAlt, FaTools } from "react-icons/fa";
 import { AiFillMessage } from "react-icons/ai";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Nav = () => {
   const [isActive, setIsActive] = useState("#");
+
+  useEffect(() => {
+    const handleScroll = () => {
+      // Calculate the scroll position and update activeTab
+      // You might need to adjust the logic based on your page structure
+      const scrollPosition = window.scrollY;
+
+      // Update activeTab based on scroll position
+      // For example, check if certain sections are in view
+      // and set the corresponding active tab
+      // ...
+      //console.log(scrollPosition);
+
+      // Example:
+      if (scrollPosition < 450) {
+        setIsActive("#");
+      } else if (scrollPosition > 844 && scrollPosition < 1450) {
+        setIsActive("#about");
+      } else if (scrollPosition > 2000 && scrollPosition < 2150) {
+        setIsActive("#experience");
+      } else if (scrollPosition > 2500 && scrollPosition < 3400) {
+        setIsActive("#services");
+      } else if (scrollPosition > 5880) {
+        setIsActive("#contact");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []); // Run the effect only once on mount
 
   return (
     <nav>
