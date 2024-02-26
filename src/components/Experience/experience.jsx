d837d53ca6324932995d0b244e97dfe0d64bd1c1
KramYengir <kramyengir@gmail.com>
2024-02-26 19:13:06 +0000

    change layout of skill items & background

--- src/components/Experience/experience.jsx
+++ src/components/Experience/experience.jsx
@@ -14,52 +14,52 @@ import { GiDeerHead } from "react-icons/gi";
 const Experience = () => {
   return (
     <section id="experience">
-      <h5>My Skills</h5>
-      <h2>My Experience</h2>
+      <p className="sub-heading">My Skills</p>
+      <h2 className="heading">My Experience</h2>
       <div className="container experience__container">
         <div className="experience__frontend">
           <h3>Frontend Development</h3>
           <div className="experience__content">
-            <article className="experience__details">
+            <div className="experience__details">
               <FaHtml5 className="experience__icon" />
-              <h4>HTML</h4>
-            </article>
-            <article className="experience__details">
+              <p>HTML</p>
+            </div>
+            <div className="experience__details">
               <FaCss3Alt className="experience__icon" />
-              <h4>CSS</h4>
-            </article>
-            <article className="experience__details">
+              <p>CSS</p>
+            </div>
+            <div className="experience__details">
               <SiSass className="experience__icon" />
-              <h4>Sass</h4>
-            </article>
-            <article className="experience__details">
+              <p>Sass</p>
+            </div>
+            <div className="experience__details">
               <IoLogoJavascript className="experience__icon" />
-              <h4>Javascript</h4>
-            </article>
-            <article className="experience__details">
+              <p>Javascript</p>
+            </div>
+            <div className="experience__details">
               <FaReact className="experience__icon" />
-              <h4>React</h4>
-            </article>
-            <article className="experience__details">
+              <p>React</p>
+            </div>
+            <div className="experience__details">
               <FaNodeJs className="experience__icon" />
-              <h4>Node.js</h4>
-            </article>
-            <article className="experience__details">
+              <p>Node.js</p>
+            </div>
+            <div className="experience__details">
               <FaCogs className="experience__icon" />
-              <h4>API (REST)</h4>
-            </article>
-            <article className="experience__details">
+              <p>API (REST)</p>
+            </div>
+            <div className="experience__details">
               <FaGitAlt className="experience__icon" />
-              <h4>GIT</h4>
-            </article>
-            <article className="experience__details">
+              <p>GIT</p>
+            </div>
+            <div className="experience__details">
               <SiJest className="experience__icon" />
-              <h4>Jest Testing</h4>
-            </article>
-            <article className="experience__details">
+              <p>Jest Testing</p>
+            </div>
+            <div className="experience__details">
               <GiDeerHead className="experience__icon" />
-              <h4>The Odin Project</h4>
-            </article>
+              <p>The Odin Project</p>
+            </div>
           </div>
         </div>
       </div>
